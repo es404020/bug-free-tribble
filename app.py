522ccad6982be57f54bd696686fae549050fbc7b
es404020 <es404020@gmail.com>
2023-05-06 22:19:15 +0100

    ol

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import ydata_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-from pycaret.regression import setup,pull,compare_models,save_model
+from pycaret.regression import setup, pull, compare_models, save_model
 
 
 # from numba.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
@@ -17,24 +17,24 @@ from pycaret.regression import setup,pull,compare_models,save_model
 
 with st.sidebar:
     st.title('RegML')
-    choice = st.radio("Navigation",["Upload","Profiling","ML","Download"])
+    choice = st.radio("Navigation", ["Upload", "Profiling", "ML", "Download"])
     st.info("This application allow you  build an automated ML pipeline using streamlit,Pandas Profiling and PyCaret")
-    
+
 
 # if os.path.exists("file.csv"):
 #     df = pd.read_csv("file.csv",index_col=None)
 #     st.dataframe(df)
 
-global df
+df = pd.DataFrame({"Name": ["Alice","Bob","Carl",],"Age": [29, 30, 31],})
 if choice == "Upload":
     st.title("Upload your Data for modelling")
     file = st.file_uploader("Upload Your Dataset here")
     if file:
-        df = pd.read_csv(file,index_col=None)
-       
-        df.to_csv("file.csv",index=None)
+        df = pd.read_csv(file, index_col=None)
+
+        df.to_csv("file.csv", index=None)
         st.dataframe(df)
-        
+
 
 if choice == "Profiling":
     st.title("Automated Exploratory Data Analysis ")
@@ -44,21 +44,20 @@ if choice == "Profiling":
 
 if choice == "ML":
     st.title("Machine learning")
-    target = st.selectbox("Pick your target variable",df.columns)
+    target = st.selectbox("Pick your target variable", df.columns)
 
-    setup(data=df,target=target,silent=True)
-    setup_df=pull()
+    setup(data=df, target=target, silent=True)
+    setup_df = pull()
     st.info("This is the ML experiment settings")
-    st.dataframe( setup_df)
-    best_model= compare_models()
-    compare_df =pull()
+    st.dataframe(setup_df)
+    best_model = compare_models()
+    compare_df = pull()
     st.info("This is the ML Model")
     st.dataframe(compare_df)
     best_model
-    save_model(best_model,'best_model')
+    save_model(best_model, 'best_model')
 
-    
 
 if choice == "Download":
-    with open("best_model.pkl",'rb') as f:
-        st.download_button("Download model",f,"trined_model.pkl")
+    with open("best_model.pkl", 'rb') as f:
+        st.download_button("Download model", f, "trined_model.pkl")
