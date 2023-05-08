6fee9246a98807b6ad1f2f5e7b6a0b3bd8deece5
es404020 <es404020@gmail.com>
2023-05-08 07:53:56 +0100

    feat

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 import pandas as pd
 # import pandas_profiling
 
-from ydata_profiling import ProfileReport
-# import ydata_profiling
-# from streamlit_pandas_profiling import st_profile_report
+# from ydata_profiling import ProfileReport
+import ydata_profiling
+from streamlit_pandas_profiling import st_profile_report
 
-# from pycaret.regression import setup, pull, compare_models, save_model
+from pycaret.regression import setup, pull, compare_models, save_model
 
 # # from numba.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
 # import warnings
@@ -37,28 +37,28 @@ if choice == "Upload":
         st.dataframe(df)
 
 
-# if choice == "Profiling":
-#     st.title("Automated Exploratory Data Analysis ")
-#     profile_report = df.profile_report()
-#     st_profile_report(profile_report)
+if choice == "Profiling":
+    st.title("Automated Exploratory Data Analysis ")
+    profile_report = df.profile_report()
+    st_profile_report(profile_report)
 
 
-# if choice == "ML":
-#     st.title("Machine learning")
-#     target = st.selectbox("Pick your target variable", df.columns)
+if choice == "ML":
+    st.title("Machine learning")
+    target = st.selectbox("Pick your target variable", df.columns)
 
-#     setup(data=df, target=target, silent=True)
-#     setup_df = pull()
-#     st.info("This is the ML experiment settings")
-#     st.dataframe(setup_df)
-#     best_model = compare_models()
-#     compare_df = pull()
-#     st.info("This is the ML Model")
-#     st.dataframe(compare_df)
-#     best_model
-#     save_model(best_model, 'best_model')
+    setup(data=df, target=target, silent=True)
+    setup_df = pull()
+    st.info("This is the ML experiment settings")
+    st.dataframe(setup_df)
+    best_model = compare_models()
+    compare_df = pull()
+    st.info("This is the ML Model")
+    st.dataframe(compare_df)
+    best_model
+    save_model(best_model, 'best_model')
 
 
-# if choice == "Download":
-#     with open("best_model.pkl", 'rb') as f:
-#         st.download_button("Download model", f, "trined_model.pkl")
+if choice == "Download":
+    with open("best_model.pkl", 'rb') as f:
+        st.download_button("Download model", f, "trined_model.pkl")
